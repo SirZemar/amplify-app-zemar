68d0884be8a182689b079e243d61f5bb10e54905
SirZemar <eduardo.w.marinho@gmail.com>
2022-12-12 16:05:48 +0000

    fix: api database authentication issues
    - Added lambda function HelloWorld

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -23,6 +23,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "helloWorld": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "storage": {
